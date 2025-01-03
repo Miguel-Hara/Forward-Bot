0b852069b7bd3072dd5611f274802441b073bf14
ᴍɪɢᴜᴇʟ ᴏ’ʜᴀʀᴀ! <anmol0700k@gmail.com>
2025-01-03 12:21:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ class Config:
     DATABASE_URI = environ.get("DATABASE_URI", "")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
     BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+    UPDATES_CHANNEL = int(environ.get("BOT_OWNER", "-1001912762300"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
